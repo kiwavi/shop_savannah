d4b86e3b50158a3f852d5c838811636cf1b3d991
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-09-21 15:08:30 +0300

    feat: basic setup of shopping app

--- shops_api/urls.py
+++ shops_api/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import shopping
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # If you're intending to use the browsable API you'll probably also want to add REST framework's login and logout views. Add the following to your root urls.py file.
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path("shopping/", include("shopping.urls")),
 ]
