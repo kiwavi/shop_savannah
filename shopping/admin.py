7713e61f9c71d24ec168348ddf58cff361cfaade
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-09-21 17:25:24 +0300

    feat: enable django-admin to see models

--- shopping/admin.py
+++ shopping/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Category, Customer, Order, OrderCategory
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderCategory)
