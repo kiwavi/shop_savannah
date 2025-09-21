0b660764df7d5a7e438883cc12cf54f66c5d2103
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-09-21 18:11:24 +0300

    feat: add some fields and validations, commit migration

--- shopping/models.py
+++ shopping/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
-
 class Product (models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
@@ -18,10 +17,24 @@ class Category (models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name="categories")
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(quantity__gt=0),
+                name="quantities_positive_int"
+            ),
+            models.CheckConstraint(
+                check=models.Q(price__gt=0),
+                name="price_positive"
+            )
+        ]
 
     def __str__(self):
         return self.name
@@ -47,6 +60,14 @@ class Order (models.Model):
    updated_at = models.DateTimeField(auto_now=True)
    categories = models.ManyToManyField(Category, through= "OrderCategory",related_name="orders")
 
+   class Meta:
+      constraints = [
+         models.CheckConstraint(
+            check=models.Q(amount__gte=0),
+            name="amount_as_positive"
+         )
+      ]
+
 class OrderCategory (models.Model):
     id = models.BigAutoField(primary_key=True)
     quantity = models.IntegerField()
@@ -63,5 +84,9 @@ class OrderCategory (models.Model):
                 fields=["customer", "order", "category"],
                 name="unique_customer_order_category",
                 condition=models.Q(order__isnull=False)   # only enforce when order_id is not null
+            ),
+            models.CheckConstraint(
+                check=models.Q(quantity__gt=0),
+                name="quantity_positive_int"
             )
         ]
