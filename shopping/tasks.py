ab9c6f50750229eab7ea69cb3201dd14075bbe7a
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-09-26 13:49:52 +0300

    Update tasks.py

--- shopping/tasks.py
+++ shopping/tasks.py
@@ -7,12 +7,11 @@ from shopping.models import Customer, Order
 import logging
 from django.core.exceptions import ObjectDoesNotExist
 
-
 @shared_task
 def send_email(order_id):
     try:
         admin = Customer.objects.get(is_superuser=True)
         order = Order.objects.get(id=order_id)
     except ObjectDoesNotExist as e
-    logger.error("Failed to send email: %s", e)
+        logger.error("Failed to send email: %s", e)
     send_mail(subject=f"New Order - #{order.id}", message=f"New order has been created. Order id ${order.id}, order details ${order.details}", from_email=settings.DEFAULT_FROM_EMAIL, recipient_list=['test@example.com'], recipient_list=['test@example.com'],)
